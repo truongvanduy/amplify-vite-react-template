5f6cbbbb126164044eac8355da93e2d3ee14f0d4
Duy <truongvanduy09072002@gmail.com>
2024-05-10 08:15:49 +0700

    fix: correct mistypo

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'file-manip-bucket',
   access: (allow) => ({
-    'psersonal-files/*': [
+    'personal-files/*': [
       allow.authenticated.to(['read', 'write', 'delete']),
       allow.entity('identity').to(['read', 'write', 'delete']),
     ],
